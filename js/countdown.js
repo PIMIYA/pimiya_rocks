0693e1e82dabd5b31149968093548742bac70fd1
HsienYu <chenghsienyu@aim.com>
2021-05-24 23:09:28 +0800

    update

--- js/countdown.js
+++ js/countdown.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   // Grab the current date
   let timeNow = new Date();
   // Target future date/24 hour time/Timezone
-  let deadline = moment.tz("2021-09-01 00:00", "Taiwan/Taipei");
+  let deadline = moment.tz("2021-09-01 00:00", "Asia/Taipei");
   // Calculate the difference in seconds between the future and current date
   let diff = deadline / 1000 - timeNow.getTime() / 1000;
   // Countdown timer
