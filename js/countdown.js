25c3e9c72bbd4625ba6f2d925fa262f963201a0b
HsienYu <chenghsienyu@aim.com>
2021-05-24 23:14:20 +0800

    update

--- js/countdown.js
+++ js/countdown.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
   // Grab the current date
   let timeNow = new Date();
   // Target future date/24 hour time/Timezone
-  let deadline = moment.tz("2021-09-01 00:00", "Asia/Taipei");
+  let deadline = moment.tz("2021-09-01 12:59", "Asia/Taipei");
   // Calculate the difference in seconds between the future and current date
   let diff = deadline / 1000 - timeNow.getTime() / 1000;
   // Countdown timer
   clock = $("#countTim").FlipClock(diff, {
-    //clockFace: "Counter",
+    clockFace: "DailyCounter",
     countdown: true,
     callbacks: {
       stop: function () {
